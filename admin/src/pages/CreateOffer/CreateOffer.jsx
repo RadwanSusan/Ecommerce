4c7a7d95f3ebb81f5a8bd03ceb76ab56c5ce6ccf
Zaid mohamad <zaidaltamari5@gmail.com>
2023-04-02 00:06:43 +0300

    admin

---
+++ admin/src/pages/CreateOffer/CreateOffer.jsx
@@ -0,0 +1,31 @@
+import "./createoffer.css";
+
+export default function CreateOffer() {
+  return (
+    <div className="newProduct">
+      <h1 className="addProductTitle">New Offer</h1>
+      <form className="addProductForm">
+        <div className="addProductItem">
+          <label>Image</label>
+          <input type="file" id="file" />
+        </div>
+        <div className="addProductItem">
+          <label>Name</label>
+          <input type="text" placeholder="Apple Airpods" />
+        </div>
+        <div className="addProductItem">
+          <label>Stock</label>
+          <input type="text" placeholder="123" />
+        </div>
+        <div className="addProductItem">
+          <label>Active</label>
+          <select name="active" id="active">
+            <option value="yes">Yes</option>
+            <option value="no">No</option>
+          </select>
+        </div>
+        <button className="addProductButton">Create</button>
+      </form>
+    </div>
+  );
+}
