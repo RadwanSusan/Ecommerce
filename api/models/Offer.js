6a3a03044982864130e9d26dedd870c11c4cd76b
radwan susan <radwansusan90@gmail.com>
2023-04-08 15:34:37 +0300

    Update

--- api/models/Offer.js
+++ api/models/Offer.js
@@ -9,13 +9,14 @@ const OfferSchema = new mongoose.Schema(
 		size: { type: Array },
 		color: { type: Array },
 		price: { type: Number, required: true },
-		originPrice: { type: Number, required: true },
-
+		originalPrice: { type: Number, required: true },
 		inStock: { type: Boolean, default: true },
 		quantity: { type: Number, required: true },
 		timeLeft: { type: String },
 		width: { type: Number, required: true },
-		height: { type: Number, required: true},
+		height: { type: Number, required: true },
+		length: { type: Number, required: true },
+		weight: { type: Number, required: true },
 	},
 	{ timestamps: true },
 );
