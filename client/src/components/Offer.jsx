ec667e321e3b7377f89cc6619f9f03740976d83d
radwan susan <radwansusan90@gmail.com>
2023-04-17 04:52:41 +0300

    Revert "⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄ ⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄ ⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⣀⡤⠤⠤⠴⠤⠤⣄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄ ⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⢠⠊⠁⠄⠄⠄⠄⠄⠄⠄⠑⡄⠄⠄⠄⠄⠄⠄⠄⠄⠄ ⠄⠄⠄⠄⠄⠄⠄⠄⠄⢠⢃⠖⠉⠉⠉⠉⠉⠉⠓⡄⠄⢸⠈⠄⠄⠄⠄⠄⠄⠄⠄ ⠄⠄⠄⠄⠄⠄⠄⠄⠄⢸⠈⠣⣀⣀⣀⣀⡠⠤⠞⠁⠄⠘⡆⠆⠄⠄⠄⠄⠄⠄⠄ ⠄⠄⠄⠄⠄⠄⠄⠄⠄⢸⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⣧⠄⠄⠄⠄⠄⠄⠄⠄ ⠄⠄⠄⠄⠄⠄⠄⠄⠄⢸⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⣿⠄⠄⠄⠄⠄⠄⠄⠄ ⠄⠄⠄⠄⠄⠄⠄⠄⠄⢸⠄⠄⢀⡄⠄⢀⣀⠄⠄⠄⠄⠄⡏⠄⠄⠄⠄⠄⠄⠄⠄ ⠄⠄⠄⠄⠄⠄⠄⠄⠄⢸⠄⠄⡏⡇⠄⠄⢳⢹⠄⠄⠄⠄⡇⠄⠄⠄⠄⠄⠄⠄⠄ ⠄⠄⠄⠄⠄⠄⠄⠄⠄⢸⠄⠄⣇⠇⠄⠄⣸⢸⠄⠄⠄⢀⡇⠄⠄⠄⠄⠄⠄⠄⠄ ⠄⠄⠄⠄⠄⠄⠄⢀⡠⠬⠭⠟⠁⢀⣠⠔⠁⠄⠉⠉⠉⠉⠄⠄⠄⠄⠄⠄⠄⠄⠄ ⠄⠄⠄⠄⠄⠄⢰⠁⠄⠄⠄⡖⠋⠁⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄ ⠄⠄⠄⠄⠄⠄⠸⡄⠄⠄⠄⠉⠒⠤⠤⠤⠤⠤⠤⠤⠤⠤⣄⠄⠄⠄⠄⠄⠄⠄⠄ ⠄⠄⠄⠄⠄⠄⠄⠈⠲⢄⣀⣀⡀⠄⠄⢀⣀⣀⣀⣀⣀⠄⠄⢱⠄⠄⠄⠄⠄⠄⠄ ⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠉⠉⠉⠉⠄⠄⠄⠄⠄⠉⢢⠄⡇⠄⠄⠄⠄⠄⠄ ⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⢀⡴⠃⣰⠃⠄⠄⠄⠄⠄⠄ ⠄⠄⠄⠄⠄⠄⠄⠄⢀⡀⠄⠄⠄⠄⠄⠄⢀⣀⠤⠒⠉⣠⠔⠁⠄⠄⠄⠄⠄⠄⠄ ⠄⠄⠄⠄⠄⠄⠄⡜⠁⠄⠉⠉⠉⠉⢉⣩⠥⠤⠒⠚⠉⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄ ⠄⠄⠄⠄⠄⠄⠄⢇⠄⠄⠄⠄⠄⠄⢇⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄ ⠄⠄⠄⠄⠄⠄⠄⠘⢆⠄⠄⠄⠄⠄⠄⠉⠒⠤⢤⣀⣀⣀⣀⠤⠔⠒⢦⠄⠄⠄⠄ ⠄⠄⠄⠄⠄⠄⠄⠄⠄⠙⠒⠒⠦⠤⠤⠤⢄⣀⣀⣀⣀⣠⠤⣄⠄⠄⠈⡆⠄⠄⠄ ⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⢣⠄⠄⢹⠄⠄⠄ ⠄⠄⠄⠄⠄⠄⠄⠄⠄⢀⣀⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⣸⠄⠄⢸⠄⠄⠄ ⠄⠄⠄⠄⠄⠄⠄⢀⠞⠁⠄⠉⠑⠒⠢⠤⠤⠤⢤⣤⡤⠤⠤⠚⠁⠄⢀⡞⠄⠄⠄ ⠄⠄⠄⠄⠄⠄⠄⡎⠄⠄⠄⠄⠄⠄⠄⣠⠤⠤⠶⠒⠒⠒⠒⠒⠒⠋⠉⠄⠄⠄⠄ ⠄⠄⠄⠄⠄⠄⠄⢣⡀⠄⠄⠄⠄⠄⠈⠳⠤⣄⣀⣀⣀⡀⠄⠄⠄⠄⠄⠄⠄⠄⠄ ⠄⠄⠄⠄⠄⠄⠄⠄⠑⢤⡀⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠉⠉⠉⠲⡄⠄⠄⠄⠄⠄ ⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠈⠉⠉⠒⠒⠒⠒⠦⠤⠤⠤⢄⣀⠄⠄⢸⠄⠄⠄⠄⠄ ⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⢈⠆⢀⡜⠄⠄⠄⠄⠄ ⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⢀⣠⢔⡫⠖⠉⠄⠄⠄⠄⠄⠄ ⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⣀⠤⠤⠤⠴⠒⠒⠚⠉⣡⠞⠁⠄⠄⠄⠄⠄⠄⠄⠄⠄ ⠄⠄⠄⠄⠄⠄⠄⠄⢀⡎⠄⠄⠄⠄⠄⠄⠄⠄⣼⠁⠄⠄⣀⣀⣀⣀⡀⠄⠄⠄⠄ ⠄⠄⠄⠄⠄⠄⠄⠄⠘⡄⠄⠄⠄⠄⠄⠄⠄⠄⠈⠉⠉⠉⠁⠄⠄⠄⠙⡆⠄⠄⠄ ⠄⠄⠄⠄⠄⠄⠄⠄⠄⠈⠲⢤⣀⣀⣀⡀⠄⢀⣀⠤⠚⠉⠉⠙⢢⠄⠄⢸⠄⠄⠄ ⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠉⠉⠁⠄⠄⠄⠄⠄⠄⣸⠄⠄⢈⡇⠄⠄ ⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⡰⠃⠄⠄⡸⠄⠄⠄ ⠄⠄⠄⠄⠄⠄⠄⢠⠦⠒⣆⣀⣀⣀⣀⣀⣀⣀⣀⣀⠤⠔⠋⠄⠄⢀⠴⠁⠄⠄⠄ ⠄⠄⠄⠄⠄⠄⢰⠃⠄⠄⠹⡀⠄⠄⠄⠄⠄⠄⠄⢀⣀⣀⠤⠤⠚⠁⠄⠄⠄⠄⠄ ⠄⠄⠄⠄⠄⠄⠄⠓⠤⠤⠤⠏⠙⠒⠒⠒⠒⠊⠉⠁⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄"
    This reverts commit 2a31640f000cff05bbeca57f79a3f70056620e4c.

--- client/src/components/Offer.jsx
+++ client/src/components/Offer.jsx
@@ -314,6 +314,19 @@ const Offer = () => {
 		if (offer.length > 0) {
 			const cartItem = mergedCart.find((item) => item._id === productId);
 			if (cartItem) {
+				console.log(
+					`🚀 ~ file: Offer.jsx:323 ~ addToCart ~ item.quantity:`,
+					item.quantity,
+				);
+				console.log(
+					`🚀 ~ file: Offer.jsx:320 ~ addToCart ~ cartItem.quantity:`,
+					cartItem.quantity,
+				);
+				if (cartItem.quantity === item.quantity) {
+					swal('Info', 'You already have the maximum amount!', 'info');
+					document.querySelector('.AddCart').disabled = true;
+					return;
+				}
 				if (cartItem.quantity + quantity <= item.quantity) {
 					dispatch(
 						addProduct({
