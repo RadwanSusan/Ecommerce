1c4b35c4b5b07f981bd7e3bde572dccbf614bf68
radwan susan <radwansusan90@gmail.com>
2023-04-24 13:56:55 +0300

    Update

--- client/src/pages/Login.jsx
+++ client/src/pages/Login.jsx
@@ -1,9 +1,9 @@
-import styled from "styled-components";
-import { mobile } from "../responsive";
-import { login } from "../redux/apiCalls";
-import { useDispatch, useSelector } from "react-redux";
-import { useState } from "react";
-import swal from "sweetalert";
+import styled from 'styled-components';
+import { mobile } from '../responsive';
+import { login } from '../redux/apiCalls';
+import { useDispatch, useSelector } from 'react-redux';
+import { useState, useEffect } from 'react';
+import swal from 'sweetalert';
 
 const Container = styled.div`
 	width: 100vw;
@@ -12,7 +12,7 @@ const Container = styled.div`
 			rgba(255, 255, 255, 0.5),
 			rgba(255, 255, 255, 0.5)
 		),
-		url("https://images.pexels.com/photos/823059/pexels-photo-823059.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1&fbclid=IwAR0zsSOLR8Rr6qk1JfnpyLxq65qil9fOZtLG99RldVesF9m-4fS_vNBr2l0")
+		url('https://images.pexels.com/photos/823059/pexels-photo-823059.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1&fbclid=IwAR0zsSOLR8Rr6qk1JfnpyLxq65qil9fOZtLG99RldVesF9m-4fS_vNBr2l0')
 			center;
 	background-size: cover;
 	display: flex;
@@ -24,7 +24,7 @@ const Wrapper = styled.div`
 	width: 400px;
 	padding: 20px;
 	background-color: white;
-	${mobile({ width: "75%" })}
+	${mobile({ width: '75%' })}
 	box-shadow: 4px 3px 13px 0px rgba(0,0,0,0.75);
 	-webkit-box-shadow: 4px 3px 13px 0px rgba(0, 0, 0, 0.75);
 	-moz-box-shadow: 4px 3px 13px 0px rgba(0, 0, 0, 0.75);
@@ -68,50 +68,54 @@ const Link = styled.a`
 	text-decoration: underline;
 	cursor: pointer;
 `;
-
 const Login = () => {
-	const [username, setUsername] = useState("");
-	const [password, setPassword] = useState("");
-	let { isFetching, error } = useSelector((state) => state.user);
+	const [username, setUsername] = useState('');
+	const [password, setPassword] = useState('');
+	const { isFetching, error } = useSelector((state) => state.user);
 	const dispatch = useDispatch();
 	const handleClick = (e) => {
 		e.preventDefault();
 		if (!username || !password) {
-			swal("Please fill in all fields");
-		}
-		login(dispatch, { username, password });
-		if (error) {
-			swal("Invalid Credentials Try Again!");
-			setUsername("");
-			setPassword("");
-			document.getElementById("username").value = "";
-			document.getElementById("password").value = "";
+			swal('Please fill in all fields');
 			return;
 		}
+		login(dispatch, { username, password });
 	};
+	useEffect(() => {
+		if (error) {
+			swal('Invalid Credentials Try Again!');
+			setUsername('');
+			setPassword('');
+			document.getElementById('username').value = '';
+			document.getElementById('password').value = '';
+		}
+	}, [error]);
 	return (
 		<Container>
 			<Wrapper>
 				<Title>SIGN IN</Title>
 				<Form>
 					<Input
-						placeholder="username"
-						id="username"
-						autoComplete="current-password"
+						placeholder='username'
+						id='username'
+						autoComplete='username'
 						onChange={(e) => setUsername(e.target.value)}
 					/>
 					<Input
-						placeholder="password"
-						id="password"
-						type="password"
-						autoComplete="current-password"
+						placeholder='password'
+						id='password'
+						type='password'
+						autoComplete='current-password'
 						onChange={(e) => setPassword(e.target.value)}
 					/>
-					<Button onClick={handleClick} disabled={isFetching}>
+					<Button
+						onClick={handleClick}
+						disabled={isFetching}
+					>
 						LOGIN
 					</Button>
-					<Link>DO NOT YOU REMEMBER THE PASSWORD?</Link>
-					<Link>CREATE A NEW ACCOUNT</Link>
+					<Link href='/forgot-password'>DO NOT YOU REMEMBER THE PASSWORD?</Link>
+					<Link href='/register'>CREATE A NEW ACCOUNT</Link>
 				</Form>
 			</Wrapper>
 		</Container>
