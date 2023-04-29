1e28476bbdc283c78d2083a585ec729ccfa0e071
Zaid mohamad <zaidaltamari5@gmail.com>
2023-04-29 14:09:40 +0300

    homelogin

--- admin/src/App.js
+++ admin/src/App.js
@@ -27,75 +27,71 @@ import { useSelector } from 'react-redux';
 import { DarkModeContext } from './context/darkModeContext';
 
 function App() {
-	const admin = useSelector((state) => state.user?.currentUser);
+	const admin = useSelector((state) => state.user?.currentUser?.isAdmin);
 	const { darkMode } = useContext(DarkModeContext);
 
 	return (
-		<div className={darkMode ? 'app dark' : 'app'}>
-			<Router>
-				{admin ? (
-					<>
-						<Topbar />
-						<div className='container'>
-							<Sidebar />
-							<Switch>
-								<Route
-									exact
-									path='/'
-								>
-									<Home />
-								</Route>
-								<Route path='/users'>
-									<UserList />
-								</Route>
-								<Route path='/user/:userId'>
-									<User />
-								</Route>
-								<Route path='/newUser'>
-									<NewUser />
-								</Route>
-								<Route path='/products'>
-									<ProductList />
-								</Route>
-								<Route path='/product/:productId'>
-									<Product />
-								</Route>
-								<Route path='/newproduct'>
-									<NewProduct />
-								</Route>
-								<Route
-									exact
-									path='/offer'
-								>
-									<Offer />
-								</Route>
-								<Route path='/offer/:offerId'>
-									<OfferUser />
-								</Route>
-								<Route path='/createOffer'>
-									<CreateOffer />
-								</Route>
-								<Route path='/transactions'>
-									<Transactions />
-								</Route>
-								<Route path='/analytics'>
-									<Analytics />
-								</Route>
-							</Switch>
-						</div>
-					</>
-				) : (
-					<Redirect to='/login' />
-				)}
-				<Route path='/forgot'>
-					<Forgot />
-				</Route>
-				<Route path='/login'>
-					<Login />
-				</Route>
-			</Router>
-		</div>
-	);
+    <div className={darkMode ? 'app dark' : 'app'}>
+      <Router>
+        <Route path='/login'>
+          {/* <Login /> */}
+          {admin ? <Redirect to='/' /> : <Login />}
+        </Route>
+        {admin ? (
+          <>
+            <Topbar />
+            <div className='container'>
+              <Sidebar />
+              <Switch>
+                <Route exact path='/'>
+                  
+                  <Home />
+                </Route>
+                <Route path='/users'>
+                  <UserList />
+                </Route>
+                <Route path='/user/:userId'>
+                  <User />
+                </Route>
+                <Route path='/newUser'>
+                  <NewUser />
+                </Route>
+                <Route path='/products'>
+                  <ProductList />
+                </Route>
+                <Route path='/product/:productId'>
+                  <Product />
+                </Route>
+                <Route path='/newproduct'>
+                  <NewProduct />
+                </Route>
+                <Route exact path='/offer'>
+                  <Offer />
+                </Route>
+                <Route path='/offer/:offerId'>
+                  <OfferUser />
+                </Route>
+                <Route path='/createOffer'>
+                  <CreateOffer />
+                </Route>
+                <Route path='/transactions'>
+                  <Transactions />
+                </Route>
+                <Route path='/analytics'>
+                  <Analytics />
+                </Route>
+              </Switch>
+            </div>
+          </>
+        ) : (
+          <Redirect to='/login' />
+        )}
+        <Route path='/forgot'>
+          <Forgot />
+        </Route>
+      </Router>
+    </div>
+  );
 }
 
 export default App;
