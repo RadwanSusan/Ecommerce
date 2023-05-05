090c593fb380c9324fdfc99bac5054eeb38cb3f5
radwan susan <radwansusan90@gmail.com>
2023-05-06 00:03:08 +0300

    Update Wishlist.jsx

--- client/src/pages/Wishlist.jsx
+++ client/src/pages/Wishlist.jsx
@@ -17,60 +17,45 @@ import axios from 'axios';
 const Container = styled.div``;
 
 const Title = styled.h1`
-  margin: 20px;
+	margin: 20px;
 `;
 
 const FilterContainer = styled.div`
-  display: flex;
-  justify-content: space-between;
+	display: flex;
+	justify-content: space-between;
 `;
 
 const Filter = styled.div`
-  margin: 20px;
-  ${mobile({ width: '0px 20px', display: 'flex', flexDirection: 'column' })}
+	margin: 20px;
+	${mobile({ width: '0px 20px', display: 'flex', flexDirection: 'column' })}
 `;
 
 const FilterText = styled.span`
-  font-size: 20px;
-  font-weight: 600;
-  margin-right: 20px;
-  ${mobile({ marginRight: '0px' })}
+	font-size: 20px;
+	font-weight: 600;
+	margin-right: 20px;
+	${mobile({ marginRight: '0px' })}
 `;
 
 const Select = styled.select`
-  padding: 10px;
-  margin-right: 20px;
-  ${mobile({ margin: '10px 0px' })}
+	padding: 10px;
+	margin-right: 20px;
+	${mobile({ margin: '10px 0px' })}
 `;
 const Option = styled.option``;
 
 const Wishlist = () => {
-  const location = useLocation();
-  const cat = location.pathname.split('/')[2];
-  const [filters, setFilters] = useState({});
-  const [sort, setSort] = useState('newest');
-  const handleFilters = (e) => {
-    const value = e.target.value;
-    setFilters({
-      ...filters,
-      [e.target.name]: value,
-    });
-  };
-  
-
-  return (
-    <Container>
-      <Announcement />
-      <Navbar />
-      <NavbarBottom />
-      <Title>Wishlist</Title>
-
-      <Woffer />
-      <Newsletter />
-      {/* <Footer /> */}
-      <FooterNew />
-    </Container>
-  );
+	return (
+		<Container>
+			<Announcement />
+			<Navbar />
+			<NavbarBottom />
+			<Title>Wishlist</Title>
+			<Woffer />
+			<Newsletter />
+			<FooterNew />
+		</Container>
+	);
 };
 
 export default Wishlist;
