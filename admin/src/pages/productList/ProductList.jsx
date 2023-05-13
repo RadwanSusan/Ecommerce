b0912c301fd91beadc55e9175866d9f7239b7b40
radwan susan <radwansusan90@gmail.com>
2023-05-13 23:50:25 +0400

    update

--- admin/src/pages/productList/ProductList.jsx
+++ admin/src/pages/productList/ProductList.jsx
@@ -1,11 +1,13 @@
-import "./productList.css";
-import { DataGrid } from "@material-ui/data-grid";
-import { DeleteOutline } from "@material-ui/icons";
-import { Link } from "react-router-dom";
-import { useEffect } from "react";
-import { useDispatch, useSelector } from "react-redux";
-import { deleteProduct, getProducts } from "../../redux/apiCalls";
-import swal from "sweetalert";
+import './productList.css';
+import { DataGrid } from '@material-ui/data-grid';
+import { DeleteOutline } from '@material-ui/icons';
+import { Link } from 'react-router-dom';
+import { useEffect } from 'react';
+import { useDispatch, useSelector } from 'react-redux';
+import { deleteProduct, getProducts } from '../../redux/apiCalls';
+import swal from 'sweetalert';
+import { CSVLink } from 'react-csv';
+
 export default function ProductList() {
 	const dispatch = useDispatch();
 	const products = useSelector((state) => state.product.products);
@@ -14,9 +16,9 @@ export default function ProductList() {
 	}, [dispatch]);
 	const handleDelete = (id) => {
 		swal({
-			title: "Are you sure?",
-			text: "Once deleted, you will not be able to recover this product!",
-			icon: "warning",
+			title: 'Are you sure?',
+			text: 'Once deleted, you will not be able to recover this product!',
+			icon: 'warning',
 			buttons: true,
 			dangerMode: true,
 		})
@@ -26,42 +28,46 @@ export default function ProductList() {
 				}
 			})
 			.catch((err) => {
-				swal("Error", err.message, "error");
+				swal('Error', err.message, 'error');
 			});
 	};
 	const columns = [
-		{ field: "_id", headerName: "ID", width: 220 },
+		{ field: '_id', headerName: 'ID', width: 220 },
 		{
-			field: "product",
-			headerName: "Product",
+			field: 'product',
+			headerName: 'Product',
 			width: 200,
 			renderCell: (params) => {
 				return (
-					<div className="productListItem">
-						<img className="productListImg" src={params.row.img} alt="" />
+					<div className='productListItem'>
+						<img
+							className='productListImg'
+							src={params.row.img}
+							alt=''
+						/>
 						{params.row.title}
 					</div>
 				);
 			},
 		},
-		{ field: "inStock", headerName: "Stock", width: 200 },
+		{ field: 'inStock', headerName: 'Stock', width: 200 },
 		{
-			field: "price",
-			headerName: "Price",
+			field: 'price',
+			headerName: 'Price',
 			width: 160,
 		},
 		{
-			field: "action",
-			headerName: "Action",
+			field: 'action',
+			headerName: 'Action',
 			width: 150,
 			renderCell: (params) => {
 				return (
 					<>
-						<Link to={"/product/" + params.row._id}>
-							<button className="productListEdit">View & Edit</button>
+						<Link to={'/product/' + params.row._id}>
+							<button className='productListEdit'>View & Edit</button>
 						</Link>
 						<DeleteOutline
-							className="productListDelete"
+							className='productListDelete'
 							onClick={() => handleDelete(params.row._id)}
 						/>
 					</>
@@ -70,32 +76,38 @@ export default function ProductList() {
 		},
 	];
 	const newObj = products.map((row) => ({ ...row }));
-  console.log(`🚀  file: OfferList.jsx:83  newObj =>`, newObj);
-  newObj.map((row) => {
-    if (row.quantity === 0) {
-      row.inStock = false;
-    }
-    return row;
-  });
-  console.log(`🚀  file: OfferList.jsx:85  newObj =>`, newObj);
+	newObj.map((row) => {
+		if (row.quantity === 0) {
+			row.inStock = false;
+		}
+		return row;
+	});
 
 	return (
-    <div className='productList'>
-      <div className='middle-product-create'>
-        <h2 className='productAddButton1'>Create Product : </h2>
-        <Link to='/newproduct'>
-          <button className='productAddButton'>Create</button>
-        </Link>
-      </div>
-      <DataGrid
-        rows={newObj}
-        disableSelectionOnClick
-        columns={columns}
-        getRowId={(row) => row._id}
-        pageSize={13}
-        autoHeight
-        rowsPerPageOptions={[5, 10, 25]}
-      />
-    </div>
-  );
+		<div className='productList'>
+			<div className='middle-product-create'>
+				<h2 className='productAddButton1'>Create Product : </h2>
+				<Link to='/newproduct'>
+					<button className='productAddButton'>Create</button>
+				</Link>
+				<CSVLink
+					className='productAddButton'
+					style={{ textDecoration: 'none', width: '100px' }}
+					data={newObj}
+					filename='products-data.csv'
+				>
+					Export to Excel
+				</CSVLink>
+			</div>
+			<DataGrid
+				rows={newObj}
+				disableSelectionOnClick
+				columns={columns}
+				getRowId={(row) => row._id}
+				pageSize={13}
+				autoHeight
+				rowsPerPageOptions={[5, 10, 25]}
+			/>
+		</div>
+	);
 }
