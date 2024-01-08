7792d9799526df9bc358c8f4279db437c7088b42
Radwan Susan <radwansusan90@gmail.com>
2024-01-08 13:27:39 +0400

    update

--- client/src/components/Announcement.jsx
+++ client/src/components/Announcement.jsx
@@ -1,20 +1,14 @@
-import React, {
-	useEffect,
-	useContext,
-	useState,
-	useCallback,
-	useMemo,
-} from 'react';
+import React, { useContext } from 'react';
 import styled from 'styled-components';
 import { mobile } from '../responsive';
 import { Link } from 'react-router-dom';
+import { useSelector, useDispatch } from 'react-redux';
 import { logoutUser } from '../redux/apiCalls';
 import { MdOutlineLightMode } from 'react-icons/md';
 import { SiDarkreader } from 'react-icons/si';
 import { DarkModeContext } from '../context/darkModeContext';
-import './announcement.css';
 import { LanguageContext } from './LanguageContext';
-
+import './announcement.css';
 const Container = styled.div`
 	height: 60px;
 	${mobile({ height: '50px' })}
@@ -22,8 +16,8 @@ const Container = styled.div`
 	@media screen and (max-width: 935px) {
 		display: none;
 	}
+	direction: ${({ language }) => (language === 'ar' ? 'rtl' : 'ltr')};
 `;
-
 const Wrapper = styled.div`
 	padding: 10px 20px;
 	display: flex;
@@ -31,25 +25,11 @@ const Wrapper = styled.div`
 	justify-content: space-between;
 	${mobile({ padding: '10px 0px' })}
 `;
-
 const Left = styled.div`
 	flex: 1;
 	display: flex;
 	align-items: center;
 `;
-
-const Language = styled.span`
-	font-size: 14px;
-	cursor: pointer;
-	${mobile({ display: 'none' })}
-`;
-
-const SearchContainer = styled.div`
-	display: flex;
-	align-items: center;
-	padding: 5px;
-`;
-
 const Right = styled.div`
 	flex: 1;
 	display: flex;
@@ -57,78 +37,40 @@ const Right = styled.div`
 	justify-content: flex-end;
 	${mobile({ flex: 2, justifyContent: 'center' })}
 `;
-
 const MenuItem = styled.div`
 	font-size: 14px;
 	cursor: pointer;
 	margin-left: 25px;
 	${mobile({ fontSize: '12px', marginLeft: '10px' })}
 `;
-const DarkMode = styled.div`
-	cursor: pointer;
-	background-color: black;
-`;
-
-const MenuItem1 = styled.div`
-	font-size: 14px;
-	cursor: pointer;
-	${mobile({ fontSize: '12px', marginLeft: '10px' })}
-`;
-/*
- * -----------------------------------1st Version--------------------------------
- */
-const Announcement = () => {
-	const { toggle, darkMode } = useContext(DarkModeContext);
-	const [tokenState, setToken] = useState();
-
-	const { language, changeLanguage } = useContext(LanguageContext);
-
-	const [tokenLoaded, setTokenLoaded] = useState(false);
-	const getToken = async () => {
-		try {
-			const token = await localStorage.getItem('persist:root');
-			if (
-				(token !== null &&
-					token !== undefined &&
-					token !== '' &&
-					JSON.parse(JSON.parse(token)?.user)?.currentUser !== undefined &&
-					JSON.parse(JSON.parse(token)?.user)?.currentUser !== null &&
-					JSON.parse(JSON.parse(token)?.user)?.username !== undefined &&
-					JSON.parse(JSON.parse(token)?.user)?.username !== null &&
-					JSON.parse(JSON.parse(token)?.user)?.username !== undefined) ||
-				JSON.parse(JSON.parse(token)?.user)?.username !== ''
-			) {
-				setToken(token);
-			}
-		} catch (error) {
-			console.error(error);
-		} finally {
-			setTokenLoaded(true);
-		}
-	};
-	useEffect(() => {
-		getToken();
-	}, []);
-	const handleLogout = () => {
-		logoutUser();
-	};
-	const Navbar = ({ isGuest }) => (
-		<Container>
+const Navbar = React.memo(
+	({ isGuest, handleLogout, darkMode, toggle, language, changeLanguage }) => (
+		<Container language={language}>
 			<Wrapper>
 				<Left>
-					<MenuItem1>Welcome to Venuse store</MenuItem1>
+					<MenuItem>
+						{language === 'en'
+							? 'Welcome to Venuse store'
+							: 'مرحبا بك في متجر فينوس'}
+					</MenuItem>
 					{!isGuest && (
 						<MenuItem>
-							<button onClick={handleLogout}>Logout</button>
+							<button onClick={handleLogout}>
+								{language === 'en' ? 'LOG OUT' : 'تسجيل الخروج'}
+							</button>
 						</MenuItem>
 					)}
 					{isGuest && (
 						<>
 							<MenuItem>
-								<Link to='/Register'>REGISTER</Link>
+								<Link to='/Register'>
+									{language === 'en' ? 'REGISTER' : 'انشاء حساب'}
+								</Link>
 							</MenuItem>
 							<MenuItem>
-								<Link to='/Login'>SIGN IN</Link>
+								<Link to='/Login'>
+									{language === 'en' ? 'SIGN IN' : 'تسجيل الدخول'}
+								</Link>
 							</MenuItem>
 						</>
 					)}
@@ -149,18 +91,36 @@ const Announcement = () => {
 						className='languageSelect'
 						value={language}
 						onChange={(e) => changeLanguage(e.target.value)}
-					>
-						<option value='en'>English</option>
-						<option value='ar'>Arabic</option>
+						style={language === 'ar' ? { marginRight: '15px' } : {}}>
+						<option value='en'>
+							{language === 'en' ? 'English' : 'الانجليزية'}
+						</option>
+						<option value='ar'>
+							{language === 'en' ? 'Arabic' : 'العربية'}
+						</option>
 					</select>
 				</Right>
 			</Wrapper>
 		</Container>
+	),
+);
+const Announcement = () => {
+	const { toggle, darkMode } = useContext(DarkModeContext);
+	const { language, changeLanguage } = useContext(LanguageContext);
+	const currentUser = useSelector((state) => state.user.currentUser);
+	const dispatch = useDispatch();
+	const handleLogout = () => {
+		dispatch(logoutUser());
+	};
+	return (
+		<Navbar
+			isGuest={!currentUser}
+			handleLogout={handleLogout}
+			darkMode={darkMode}
+			toggle={toggle}
+			language={language}
+			changeLanguage={changeLanguage}
+		/>
 	);
-	if (!tokenLoaded) {
-		return null;
-	}
-	return tokenState ? <Navbar isGuest={false} /> : <Navbar isGuest={true} />;
 };
-
 export default Announcement;
