95a2c7732eef6a04e14c9ef96764f23178f62762
Radwan Susan <radwansusan90@gmail.com>
2024-03-15 14:23:50 +0400

    update

--- admin/vite.config.js
+++ admin/vite.config.js
@@ -5,6 +5,7 @@ import nodeGlobalsPolyfill from '@esbuild-plugins/node-globals-polyfill';
 import dsv from '@rollup/plugin-dsv';
 export default defineConfig({
 	server: {
+		host: true,
 		port: 5001,
 	},
 	build: {
