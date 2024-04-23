ad246dd759725c03f51e918a88318db9b173dd70
Zaid mohamad <93926834+ZaidAltamari@users.noreply.github.com>
2024-04-23 11:52:07 +0400

    new

--- admin/vite.config.js
+++ admin/vite.config.js
@@ -1,66 +1,8 @@
-// import { defineConfig } from 'vite';
-// import react from '@vitejs/plugin-react';
-// import nodeGlobalsPolyfill from '@esbuild-plugins/node-globals-polyfill';
-
-// import dsv from '@rollup/plugin-dsv';
-// export default defineConfig({
-// 	server: {
-// 		host: true,
-// 		port: 5001,
-// 	},
-// 	build: {
-// 		outDir: 'build',
-// 		chunkSizeWarningLimit: 1600,
-// 		rollupOptions: {
-// 			output: {
-// 				manualChunks(id) {
-// 					if (id.includes('node_modules')) {
-// 						return id.split('node_modules/')[1].split('/')[0];
-// 					}
-// 				},
-// 			},
-// 		},
-// 	},
-// 	plugins: [
-// 		react(),
-// 		nodeGlobalsPolyfill(),
-// 		dsv(),
-// 		nodeGlobalsPolyfill({
-// 			process: true,
-// 			buffer: true,
-// 		}),
-// 	],
-// 	optimizeDeps: {
-// 		esbuildOptions: {
-// 			define: {
-// 				global: 'globalThis',
-// 				process: 'globalThis.process',
-// 			},
-// 			plugins: [
-// 				nodeGlobalsPolyfill({
-// 					process: true,
-// 					buffer: true,
-// 				}),
-// 			],
-// 		},
-// 	},
-// 	resolve: {
-// 		alias: {
-// 			process: 'process/browser',
-// 			stream: 'stream-browserify',
-// 			zlib: 'browserify-zlib',
-// 			util: 'util',
-// 		},
-// 	},
-// 	define: {
-// 		'process.env': {},
-// 	},
-// });
 import { defineConfig } from 'vite';
-// import react from '@vitejs/plugin-react'; // Removed this line
+import react from '@vitejs/plugin-react';
 import nodeGlobalsPolyfill from '@esbuild-plugins/node-globals-polyfill';
-import dsv from '@rollup/plugin-dsv';
 
+import dsv from '@rollup/plugin-dsv';
 export default defineConfig({
 	server: {
 		host: true,
@@ -80,10 +22,13 @@ export default defineConfig({
 		},
 	},
 	plugins: [
-		// react(), // Removed this line
+		react(),
 		nodeGlobalsPolyfill(),
 		dsv(),
-		nodeGlobalsPolyfill({ process: true, buffer: true }),
+		nodeGlobalsPolyfill({
+			process: true,
+			buffer: true,
+		}),
 	],
 	optimizeDeps: {
 		esbuildOptions: {
@@ -91,7 +36,12 @@ export default defineConfig({
 				global: 'globalThis',
 				process: 'globalThis.process',
 			},
-			plugins: [nodeGlobalsPolyfill({ process: true, buffer: true })],
+			plugins: [
+				nodeGlobalsPolyfill({
+					process: true,
+					buffer: true,
+				}),
+			],
 		},
 	},
 	resolve: {
