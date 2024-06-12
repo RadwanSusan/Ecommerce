f627806be1565f95458968d7167b849e57187ef0
Zaid mohamad <93926834+ZaidAltamari@users.noreply.github.com>
2024-06-12 16:25:05 +0400

    new

--- client/vite.config.js
+++ client/vite.config.js
@@ -1,3 +1,59 @@
+// import { defineConfig } from 'vite';
+// import react from '@vitejs/plugin-react';
+// import nodeGlobalsPolyfill from '@esbuild-plugins/node-globals-polyfill';
+
+// export default defineConfig({
+// 	server: {
+// 		host: true,
+// 		port: 2066,
+// 	},
+// 	build: {
+// 		outDir: 'build',
+// 		chunkSizeWarningLimit: 1600,
+// 		rollupOptions: {
+// 			output: {
+// 				manualChunks(id) {
+// 					if (id.includes('node_modules')) {
+// 						return id.split('node_modules/')[1].split('/')[0];
+// 					}
+// 				},
+// 			},
+// 		},
+// 	},
+// 	plugins: [
+// 		react(),
+// 		nodeGlobalsPolyfill(),
+// 		nodeGlobalsPolyfill({
+// 			process: true,
+// 			buffer: true,
+// 		}),
+// 	],
+// 	optimizeDeps: {
+// 		esbuildOptions: {
+// 			define: {
+// 				global: 'globalThis',
+// 				process: 'globalThis.process',
+// 			},
+// 			plugins: [
+// 				nodeGlobalsPolyfill({
+// 					process: true,
+// 					buffer: true,
+// 				}),
+// 			],
+// 		},
+// 	},
+// 	resolve: {
+// 		alias: {
+// 			process: 'process/browser',
+// 			stream: 'stream-browserify',
+// 			zlib: 'browserify-zlib',
+// 			util: 'util',
+// 		},
+// 	},
+// 	define: {
+// 		'process.env': {},
+// 	},
+// });
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 import nodeGlobalsPolyfill from '@esbuild-plugins/node-globals-polyfill';
@@ -15,14 +71,15 @@ export default defineConfig({
 				manualChunks(id) {
 					if (id.includes('node_modules')) {
 						return id.split('node_modules/')[1].split('/')[0];
+					} else {
+						return 'bundle';
 					}
 				},
 			},
 		},
 	},
 	plugins: [
-		react(),
-		nodeGlobalsPolyfill(),
+		react({ refresh: false }),
 		nodeGlobalsPolyfill({
 			process: true,
 			buffer: true,
@@ -40,14 +97,6 @@ export default defineConfig({
 					buffer: true,
 				}),
 			],
-		},
-	},
-	resolve: {
-		alias: {
-			process: 'process/browser',
-			stream: 'stream-browserify',
-			zlib: 'browserify-zlib',
-			util: 'util',
 		},
 	},
 	define: {
