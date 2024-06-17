f6da4c8406e9e753e90de299b36447de57da25af
Zaid mohamad <93926834+ZaidAltamari@users.noreply.github.com>
2024-06-17 17:05:52 +0400

    new

--- admin/src/requestMethods.js
+++ admin/src/requestMethods.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 const BASE_URL = 'http://194.195.86.67:4000/api/';
 
-const user = JSON.parse(localStorage.getItem('persist:root') || '')?.user;
+const user = JSON.parse(localStorage.getItem('persist:root'))?.user;
 const currentUser = user && JSON.parse(user).currentUser;
 const TOKEN = currentUser?.accessToken;
 export const publicRequest = axios.create({
